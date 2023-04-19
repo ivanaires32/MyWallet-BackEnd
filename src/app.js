dc9932777ad2f41b3f6670917ee736421ff712d6
Ivan <ivanaires32@gmail.com>
2023-04-18 22:29:21 -0300

    sistema de login e cadastro

--- src/app.js
+++ src/app.js
@@ -2,6 +2,9 @@ import express, { json } from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import { MongoClient } from "mongodb"
+import joi from "joi"
+import bcrypt from "bcrypt"
+import { v4 as uuid } from "uuid"
 
 const app = express()
 
@@ -22,16 +25,26 @@ try {
 
 const db = mongoCliente.db()
 
+const userSchema = joi.object({
+    name: joi.string().required(),
+    email: joi.string().required().email(),
+    password: joi.string().required().min(3)
+})
+
 app.post("/newUser", async (req, res) => {
     const { name, email, password } = req.body
 
-    if (!name || !email || !password) return res.status(422).send("Todos os campos são obrigatorios")
-    if (password.length < 3) return res.status(422).send("Senha muito curta")
+    const validation = userSchema.validate(req.body, { abortEarly: false })
+    if (validation.error) {
+        return res.status(422).send(validation.error.details.map(d => d.message))
+    }
 
     try {
-        const unserOn = await db.collection("users").findOne({ name })
+        const unserOn = await db.collection("users").findOne({ email })
         if (unserOn) return res.status(409).send("Usuario já cadastrado")
-        await db.collection("users").insertOne({ name, email, password })
+
+        const hash = bcrypt.hashSync(password, 10)
+        await db.collection("users").insertOne({ name, email, password: hash })
 
         res.sendStatus(201)
     } catch (err) {
@@ -41,16 +54,30 @@ app.post("/newUser", async (req, res) => {
 
 app.post("/sign-in", async (req, res) => {
     const { email, password } = req.body
+    const token = uuid()
 
     if (!email || !password) return res.status(422).send("Campos obrigatorios")
 
     try {
         const user = await db.collection("users").findOne({ email })
-        if (!user) return res.status(404).send("Usuario não encontrado")
-        if (user.password !== password) return res.status(401).send("Senha incorreta")
-        res.sendStatus(201)
+        if (user && bcrypt.compareSync(password, user.password)) {
+            await db.collection("sessoes").insertOne({ token, idUser: user._id })
+            res.status(200).send(token)
+        } else {
+            res.status(404).send("Email ou senha incorret")
+        }
     } catch (err) {
         res.status(500).send(err.message)
     }
 })
+
+app.get("/home", async (req, res) => {
+    const { autorization } = req.body
+    try {
+
+    } catch (err) {
+
+    }
+})
+
 app.listen(5000, () => console.log("Servidor rodando"))
