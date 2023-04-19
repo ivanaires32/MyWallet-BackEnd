9ff9686c4c242034d0f1b046ed39028b45a4ea4a
Ivan <ivanaires32@gmail.com>
2023-04-19 14:39:09 -0300

    saldo usuario

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import { MongoClient, ObjectId } from "mongodb"
 import joi from "joi"
 import bcrypt from "bcrypt"
 import { v4 as uuid } from "uuid"
+import dayjs from "dayjs"
 
 const app = express()
 
@@ -56,7 +57,7 @@ app.post("/sign-in", async (req, res) => {
     const { email, password } = req.body
 
 
-    if (!email || !password) return res.status(422).send("Campos obrigatorios")
+    if (!email || !password) return res.status(422).send("Todos os campos obrigatorios")
 
     try {
         const user = await db.collection("users").findOne({ email })
@@ -81,15 +82,51 @@ app.get("/home", async (req, res) => {
     try {
         const sessao = await db.collection("sessoes").findOne({ token })
         if (!sessao) return res.sendStatus(404)
-        const user = await db.collection("users").findOne({ _id: new ObjectId(sessao.idUser) })
+
+        const user = await db.collection("users").find({ _id: new ObjectId(sessao.idUser) }).toArray()
         if (!user) return res.sendStatus(404)
         delete user.password
-        res.status(200).send(user)
+
+        const dadosTransacao = await db.collection("transacoes").find({ idUser: sessao.idUser }).toArray()
+        if (!dadosTransacao) res.status(200).send(user)
+
+        res.status(200).send(dadosTransacao)
     } catch (err) {
         res.status(500).send(err.message)
     }
 })
 
-app.post("")
+app.post("/nova-transacao/:tipo", async (req, res) => {
+    const { tipo } = req.params
+    const { value, description } = req.body
+    const { authorization } = req.headers
+    const token = authorization?.replace("Bearer ", "")
+    const formatValue = value.replace(",", ".")
+    const valor = Number(formatValue)
 
+    if (!value || !description) return res.status(422).send("Todos os campos são obrigatorios")
+
+    const valueSchema = joi.object({
+        valor: joi.number().required(),
+        description: joi.string().required()
+    })
+
+    const validation = valueSchema.validate({ valor, description }, { abortEarly: false })
+    if (validation.error) return res.status(422).send(validation.error.details.map(d => d.message))
+
+    try {
+        const sessao = await db.collection("sessoes").findOne({ token })
+        if (!sessao) return res.sendStatus(401)
+
+        const user = await db.collection("users").findOne({ _id: new ObjectId(sessao.idUser) })
+        if (!user) return res.sendStatus(422)
+        delete user.password
+
+        await db.collection("transacoes").insertOne({ idUser: sessao.idUser, name: user.name, value: valor, description, type: tipo, day: dayjs().format("DD/MM") })
+
+        res.sendStatus(201)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
+})
 app.listen(5000, () => console.log("Servidor rodando"))
